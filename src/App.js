a5259992c43a592cf02b8e4cf40c45459b7efad7
Sahil Rajput <sahilrajput03@gmail.com>
2022-08-06 19:48:54 +0530

    Add example from docs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+// https://github.com/typicode/lowdb/tree/main/examples#browser
+import {LowSync, LocalStorage} from 'lowdb'
+
+const adapter = new LocalStorage('db')
+const db = new LowSync(adapter)
+
+db.read()
+db.data ||= {posts: []}
+
+db.data.posts.push({title: 'lowdb'})
+
+db.write()
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div className='App'></div>
 }
 
-export default App;
+export default App
